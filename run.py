afde76254eeeed3d7fe83c209a1ff34b9bc099bb
ProgramCodeMasterA <vb7zl582@students.codeinstitute.net>
2025-04-01 19:18:18 +0100

    Add requirements to project for deployment

--- run.py
+++ run.py
@@ -1,12 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-# pprint not needed when deployed to Heroku environment
-from pprint import pprint
-
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
